72451ab0c99a282d7e5767c23ddaec0d3f20396b
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-03-17 02:32:15 -0400

    onbaording username validation route

--- server/user/src/routes/onboarding.route.js
+++ server/user/src/routes/onboarding.route.js
@@ -3,30 +3,45 @@ const router = express.Router()
 const { userMustSignUp } = require('../middleware/requireLogin') 
 const { UserCreatedPublisher } = require('../events/publishers/user-created-publisher')
 const nats =  require('../nats')
+const { currentUser, requireAuth } = require('@pgcomm/common')
+const User = require('../models/User')
+
 
 router.get('/signup',userMustSignUp, async (req, res) => {
-// await Program.create({
-//     programName: "Comp Sci",
-//     programDescription: "coding tings"
-//   })
-//   const temp = []
-//   const findmajor = await Program.findOne({programName: "Comp Sci"})
-//   temp.push(findmajor)
-//   await User.findOneAndUpdate({googleId:req.user.googleId}, {
-//     programsFollowing: temp,
-//     newRegister: true
-//   })
-//   console.log(findmajor)
-//   console.log(req.user)
+
    res.send(req.user)
   
 })
 
-router.post('/api/user/signup', (req, res) => {
+router.post('/api/user/username', async (req, res) => {
+   
+   const { username } = req.body
+   const username_aplhanumeric_only = /^([a-zA-Z0-9_-]+)$/
+   const existing_username = await User.findOne({username: username})
 
-   // save user profile changes
+   if(!username){
+      return res.status(422).json({error: 'Username must be between 3 and 20 characters'})
+   }
 
-   new UserCreatedPublisher(nats.client).publish(req.user)
+   if(!username_aplhanumeric_only.test(username)){
+      return res.status(422).json({error:'Letters, numbers, dashes, and underscores only. Please try again without symbols.'})
+   }
+   
+   if(username.length < 3 || username.length > 20){
+      return res.status(422).json({error: 'Username must be between 3 and 20 characters'})
+   }
+
+   if(existing_username){
+      return res.status(422).json({error: 'That username is already taken'})
+   }
+
+})
+
+
+router.post('/api/user/join', currentUser, requireAuth, (req, res) => {
+
+   // publish user:created event 
+   new UserCreatedPublisher(nats.client).publish(req.currentUser)
 
 })
 
