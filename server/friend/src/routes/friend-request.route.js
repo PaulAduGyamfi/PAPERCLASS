3eb59ba13740bb782b13403af649e61d44aaa815
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-03-27 21:34:58 -0400

    postgres queries

--- server/friend/src/routes/friend-request.route.js
+++ server/friend/src/routes/friend-request.route.js
@@ -2,9 +2,20 @@ const express = require('express')
 const router = express.Router()
 const postgres = require('../postgres')
 
-router.post('/f/status/r', (req, res) => {
-    const { id } = req.body
+router.post('/f/status/r', async (req, res) => {
+    const { requester_id, addressee_id } = req.body
 
+    const text = 'INSERT INTO relationship (requester_id, addressee_id, status_code, action_user_id) VALUES ($1, $2, $3, $4)'
+    const values = [requester_id, addressee_id, '0', requester_id]
+
+    try {
+        const queryRes = await postgres.client.query(text, values)
+        res.status(200).json(queryRes)
+        
+    } catch (err) {
+        console.log(err.stack)
+        res.status(422)
+    }
 })
 
 module.exports = router
