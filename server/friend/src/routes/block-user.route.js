51c6063e9560fdcc18735191e1290a5d1c257404
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-03-30 00:47:45 -0400

    block user query/route

--- server/friend/src/routes/block-user.route.js
+++ server/friend/src/routes/block-user.route.js
@@ -2,8 +2,33 @@ const express = require('express')
 const router = express.Router()
 const postgres = require('../postgres')
 
-router.post('/f/status/b', (req, res) => {
-    const { id } = req.body
+router.post('/f/status/b', async (req, res) => {
+    const { requester_id, addressee_id } = req.body
+
+    const text = 'INSERT INTO relationship (requester_id, addressee_id, status_code, action_user_id, specified_date_time) VALUES ($1, $2, $3, $4, $5)'
+    const values = [requester_id, addressee_id, '3', requester_id, 'now()']
+
+    const exist_text = 'SELECT EXISTS(SELECT requester_id, addressee_id FROM relationship WHERE ((requester_id = $1 AND addressee_id = $2) OR (requester_id = $2 AND addressee_id = $1)))'
+    const exist_values = [requester_id, addressee_id]
+
+    const block_text = 'UPDATE relationship SET status_code = $3, action_user_id = $1, specified_date_time = $4 WHERE (requester_id = $1 AND addressee_id = $2) OR (requester_id = $2 AND addressee_id = $1)'
+    const block_values = [requester_id, addressee_id, '3', 'now()']
+
+    try {
+        const response = await postgres.client.query(exist_text, exist_values)
+
+        if(response.rows[0].exists){
+            const acceptQuery = await postgres.client.query(block_text, block_values)
+            return res.status(200).send(acceptQuery)
+        }
+        else{
+            const blockQuery = await postgres.client.query(text, values)
+            return res.status(200).send(blockQuery)
+        }
+    } catch (err) {
+        console.log(err.stack)
+        res.status(422)
+    }
     
 })
 
