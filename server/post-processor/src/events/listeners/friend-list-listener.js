d62f417f04e171c39a5df1f1a1db3b7cb6283b28
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-04-02 23:09:48 -0400

    append post in redis message box/set

--- server/post-processor/src/events/listeners/friend-list-listener.js
+++ server/post-processor/src/events/listeners/friend-list-listener.js
@@ -1,12 +1,25 @@
 const { Listener, Subjects } = require('@pgcomm/common')
 const { queueGrouName } = require('./queue-group-name')
+const redis = require('../../redis')
 
 class FriendListListener extends Listener {
   _subject = Subjects.FriendList
   _queueGroupName = queueGrouName
 
   async onMessage(data, msg) {
-    console.log(data)
+     console.log(data)
+
+    data.friendlist.forEach((msgbox) => {
+      const key = msgbox
+      const member = data.post_id
+      redis.client.SADD(key, member)
+    })
+  
+    // TEST QUERY
+    // redis.client.SMEMBERS("6066a0eb304eab1ab26768a6", (err, res) => {
+    //   const result = res
+    //   console.log(result)
+    // })
 
     msg.ack()
   }
