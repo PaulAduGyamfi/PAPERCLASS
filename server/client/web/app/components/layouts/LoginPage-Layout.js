b6e985b54eab4f6f6ba2b7cdd1b1a330ecaed5c0
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-04-13 17:43:51 -0400

    added scss

--- server/client/web/app/components/layouts/LoginPage-Layout.js
+++ server/client/web/app/components/layouts/LoginPage-Layout.js
@@ -1,9 +1,9 @@
-import LoginButton from '../modules/LoginButton'
+import LoginButton from '../modules/LoginButton/LoginButton'
 
 const LoginPageLayout = () => {
   return (
     <div>
-      <h1>WELCOME!!</h1>
+      <h1>WELCOME!</h1>
       <LoginButton />
     </div>
   )
