cdda530f7a97f5b147915ea723a9f581a8098e2c
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-04-18 03:40:35 -0400

    refactored app.js

--- server/client/mobile/App.js
+++ server/client/mobile/App.js
@@ -1,47 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  Alert,
-  Button,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
 
 import LoginScreenTemplate from './src/app/components/templates/LoginScreen-Template';
 
 const App = () => {
   return <LoginScreenTemplate />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
