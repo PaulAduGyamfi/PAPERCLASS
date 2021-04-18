72c90c6cda64fe6aabf408e04ca13e80b22017d0
Paul Adu-Gyamfi <pjadu7@gmail.com>
2021-04-17 20:46:15 -0400

    mobile login screen styling

--- server/client/mobile/src/app/components/layouts/LoginScreen-Layout/LoginScreen-Layout.js
+++ server/client/mobile/src/app/components/layouts/LoginScreen-Layout/LoginScreen-Layout.js
@@ -8,10 +8,21 @@ const LoginScreenLayout = () => {
   return (
     <View style={styles.loginScreenLayoutContainer}>
       <View style={styles.loginScreenLayoutTopConatainer}>
-        <Text>Hi</Text>
+        <Text>[ Logo Here ]</Text>
       </View>
       <View style={styles.loginScreenLayoutBottomContainer}>
-        <LoginButton />
+        <View style={styles.loginScreenLayoutBottomModalContainer}>
+          <View style={styles.loginScreenLayoutBottomModalWrapper}>
+            <View style={styles.loginScreenLayoutBottomModalHeader}>
+              <Text>Welcome to Brand Name</Text>
+              <Text>Get started with your account</Text>
+            </View>
+            <LoginButton />
+            <Text style={styles.loginScreenLayoutBottomModalFooter}>
+              By signing up you agree to our Terms of Service and Privacy Policy
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
   );
--- server/client/mobile/src/app/components/layouts/LoginScreen-Layout/LoginScreen-Layout.styles.js
+++ server/client/mobile/src/app/components/layouts/LoginScreen-Layout/LoginScreen-Layout.styles.js
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
 
   loginScreenLayoutTopConatainer: {
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: 'lime',
     minHeight: '50%',
     width: '100%',
@@ -23,6 +24,33 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '50%',
     backgroundColor: '#f0fc',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loginScreenLayoutBottomModalContainer: {
+    width: '95%',
+    height: 330,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    marginBottom: 35,
+  },
+
+  loginScreenLayoutBottomModalWrapper: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+
+  loginScreenLayoutBottomModalHeader: {
+    alignItems: 'center',
+  },
+
+  loginScreenLayoutBottomModalFooter: {
+    textAlign: 'center',
   },
 });
 
